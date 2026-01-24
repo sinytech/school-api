4211a9a29dd03415aee9155e8875a71ee1037d76
Alexander Vrublevsky <alex.vrublevsky@gmail.com>
2026-01-24 19:07:42 +0300

    Classes & Marks database entities

--- app/models/models.py
+++ app/models/models.py
@@ -1,5 +1,8 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
 from app.database import Base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 
 class User(Base):
     __tablename__ = 'users'
@@ -9,4 +12,40 @@ class User(Base):
     name = Column(String, nullable = False)
     form = Column(String, nullable = False)
     year =  Column(Integer, nullable = False)
+    
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
+
+
+class Class(Base):
+    __tablename__ = 'classes'
+
+    id = Column(Integer, primary_key = True, nullable = False)
+    title = Column(String, nullable = False)
+
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
+
+
+class Mark(Base):
+    __tablename__ = 'marks'
+
+    id = Column(Integer, primary_key = True, nullable = False)
+    user_id = Column(Integer, ForeignKey(
+                            "users.id", ondelete="CASCADE"), nullable=False)
+    class_id = Column(Integer, ForeignKey(
+                            "classes.id", ondelete="CASCADE"), nullable=False)
+    mark = Column(Integer, nullable = False)
+    mark_ref_id = Column(Integer, ForeignKey(
+                            "marks.id", ondelete="CASCADE"), nullable=True)
+
+
+    published_at = Column(TIMESTAMP(timezone=True), nullable = True)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
+
+    pupil   = relationship("User")
+    cls     = relationship("Class", )
+    mark_ref = relationship("Mark", remote_side=['id'], backref='ref_marks')
+
 
